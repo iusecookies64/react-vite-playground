b0bb2233d55981bfe938cc1be0b887ae659d359c
Tushar <traghav64@gmail.com>
2024-01-15 01:51:51 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useMemo, useRef, useEffect } from "react";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+    const [inputValue, setInputValue] = useState("");
+    const [users, setUsers] = useState([]);
 
-	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+    const filteredUsers = useMemo(() => {
+        // logic to filter users list by name
+    }, [inputValue]);
+
+    function onChangeHandler() {
+        // logic to to set inputValue
+    }
+
+    useEffect(() => {
+        // logic to fetch user details using https://jsonplaceholder.typicode.com/users api
+    }, []);
+
+    return (
+        <div className="app">
+            <input
+                type="text"
+                placeholder="Search User By Name"
+                onChange={onChangeHandler}
+            ></input>
+
+            <div className="users-container">
+                {/* Logic to render users to show user details */}
+            </div>
+        </div>
+    );
 }
 
 export default App;
